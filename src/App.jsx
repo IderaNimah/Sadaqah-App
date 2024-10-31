1b5c1c765e3a850294962f1c56880507d4270d56
Idera Nimah <iderayusuf29@gmail.com>
2024-10-31 12:24:11 +0100

    header

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,38 @@ function App() {
   
   return (
     <>
-      
+      <Header/>
     </>
+  )
+}
+
+function Header(){
+  return (
+     <div className="header">
+     <div className="logo-nav">
+      <div className='logo'>
+     <h1>Sadaqah</h1>
+     </div>
+     <nav>
+      <ul>
+        <li>Home</li>
+        <li>Explore</li>
+        <li>Add Card</li>
+      </ul>
+     </nav>
+     </div>
+
+     <div className="notif">
+      <input type="text" placeholder='Search' />
+      <img src="./assets/notification.png" alt="notification" />
+     </div>
+
+     <div className="profile-header">
+      <img src="./assets/Profileheader.png" alt="profileheader" />
+      <p>ijtas_muiz</p>
+     </div>
+     </div>
+
   )
 }
 
