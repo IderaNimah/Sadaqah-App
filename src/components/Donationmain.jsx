81d7de2d87abc37809cb1a5a2028f27934448d1f
Idera Nimah <iderayusuf29@gmail.com>
2024-11-28 17:13:28 +0100

    new update

--- src/components/Donationmain.jsx
+++ src/components/Donationmain.jsx
@@ -1,88 +1,79 @@
+import PropTypes from "prop-types";
+
+const donationGroupOne = [
+  {
+    name: "Developers boys from GR",
+    ongoing: "24 ongoing donations",
+    number: "80",
+    headerImg: "./assets/collationheader.png",
+    footerImg: "./assets/collation.png",
+  },
+  {
+    name: "Alabi, James and co",
+    ongoing: "2 ongoing donations",
+    number: "5",
+    headerImg: "./assets/collabtwo.png",
+    footerImg: "./assets/collationtwo.png",
+  },
+  {
+    name: "Gather Together",
+    ongoing: "4 ongoing donations",
+    number: "12",
+    headerImg: "./assets/collabthree.png",
+    footerImg: "./assets/collationthree.png",
+  },
+  {
+    name: "Donation Group for Mosq..",
+    ongoing: "6 ongoing donations",
+    number: "10",
+    headerImg: "./assets/collabfour.png",
+    footerImg: "./assets/collationfour.png",
+  },
+];
+
+const donationGroupTwo = [
+  {
+    name: "Hospitals Donation",
+    ongoing: "10 ongoing donations",
+    number: "34",
+    headerImg: "./assets/collabfive.png",
+    footerImg: "./assets/collation.png",
+  },
+  {
+    name: "Mosque IB Central",
+    ongoing: "5 ongoing donations",
+    number: "45",
+    headerImg: "./assets/collabsix.png",
+    footerImg: "./assets/collationtwo.png",
+  },
+  {
+    name: "Alabi, James and co",
+    ongoing: "2 ongoing donations",
+    number: "5",
+    headerImg: "./assets/collabtwo.png",
+    footerImg: "./assets/collationthree.png",
+  },
+  {
+    name: "Hospitals Donation",
+    ongoing: "10 ongoing donations",
+    number: "34",
+    headerImg: "./assets/collabfive.png",
+    footerImg: "./assets/collation.png",
+  },
+];
+
 function DonationMain() {
   return (
     <>
       <div className="donation-container">
-        <Donationgroup
-          donor={{
-            name: "Developers boys from GR",
-            ongoing: "24 ongoing donations",
-            number: "80",
-            headerImg: "./assets/collationheader.png",
-            footerImg: "./assets/collation.png",
-          }}
-        />
-
-        <Donationgroup
-          donor={{
-            name: "Alabi, James and co",
-            ongoing: "2 ongoing donations",
-            number: "5",
-            headerImg: "./assets/collabtwo.png",
-            footerImg: "./assets/collationtwo.png",
-          }}
-        />
-
-        <Donationgroup
-          donor={{
-            name: "Gather Together",
-            ongoing: "4 ongoing donations",
-            number: "12",
-            headerImg: "./assets/collabthree.png",
-            footerImg: "./assets/collationthree.png",
-          }}
-        />
-
-        <Donationgroup
-          donor={{
-            name: "Donation Group for Mosq..",
-            ongoing: "6 ongoing donations",
-            number: "10",
-            headerImg: "./assets/collabfour.png",
-            footerImg: "./assets/collationfour.png",
-          }}
-        />
+        {donationGroupOne.map((donor, index) => (
+          <Donationgroup key={index} donor={donor} />
+        ))}
       </div>
-
       <div className="donation-container-two">
-        <Donationgroup
-          donor={{
-            name: "Hospitals Donation",
-            ongoing: "10 ongoing donations",
-            number: "34",
-            headerImg: "./assets/collabfive.png",
-            footerImg: "./assets/collation.png",
-          }}
-        />
-
-        <Donationgroup
-          donor={{
-            name: "Mosque IB Central",
-            ongoing: "5 ongoing donations",
-            number: "45",
-            headerImg: "./assets/collabsix.png",
-            footerImg: "./assets/collationtwo.png",
-          }}
-        />
-
-        <Donationgroup
-          donor={{
-            name: "Alabi, James and co",
-            ongoing: "2 ongoing donations",
-            number: "5",
-            headerImg: "./assets/collabtwo.png",
-            footerImg: "./assets/collationthree.png",
-          }}
-        />
-
-        <Donationgroup
-          donor={{
-            name: "Hospitals Donation",
-            ongoing: "10 ongoing donations",
-            number: "34",
-            headerImg: "./assets/collabfive.png",
-            footerImg: "./assets/collation.png",
-          }}
-        />
+        {donationGroupTwo.map((donor, index) => (
+          <Donationgroup key={index} donor={donor} />
+        ))}
       </div>
     </>
   );
@@ -92,16 +83,29 @@ function Donationgroup({ donor }) {
   return (
     <section className="profile">
       <div className="coll-one">
-        <img src={donor.headerImg} alt="" />
+        <img src={donor.headerImg} alt={`${donor.name} header`} />
         <h3>{donor.name}</h3>
       </div>
       <p>{donor.ongoing}</p>
       <div>
-        <p>People joined:{donor.number}</p>
-        <img src={donor.footerImg} alt="" />
+        <p>People joined: {donor.number}</p>
+        <img src={donor.footerImg} alt={`${donor.name} footer`} />
       </div>
     </section>
   );
 }
+
+
+
+// PropTypes validation
+Donationgroup.propTypes = {
+  donor: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    ongoing: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+    headerImg: PropTypes.string.isRequired,
+    footerImg: PropTypes.string.isRequired,
+  }).isRequired,
+};
 
 export default DonationMain;
